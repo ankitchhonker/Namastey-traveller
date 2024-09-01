56db05bb16696a40b96a20ee310c75461b560f5d
Ankit chhonker <160047114+ankitchhonker@users.noreply.github.com>
2024-09-01 09:54:24 +0530

    Update listing.js

--- models/listing.js
+++ models/listing.js
@@ -48,11 +48,10 @@ const listSchema = new Schema({
             type: {
               type: String, // Don't do `{ location: { type: String } }`
               enum: ['Point'], // 'location.type' must be 'Point'
-              required: true,
+              
             },
             coordinates: {
-              type: [Number],
-              required: true,
+              type: [Number]
             },
           },
         category:{
